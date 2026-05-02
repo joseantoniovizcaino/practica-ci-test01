b0d32d0928276c1950c5d7cb51d4314a223e8a69
joseantoniovizcaino <josvizmil@alu.edu.gva.es>
2026-05-02 21:34:47 +0200

    cambiado para que funcione mocha

--- tests/espar.js
+++ tests/espar.js
@@ -1,22 +1,20 @@
+const assert = require('assert');
 const esPar = require('../match');
 
-// Prueba 1: Es par?
-if (esPar(4) !== true) {
-  console.error("Error: 4 debería ser par");
-  process.exit(1);
-}
+describe('Módulo match', () => {
+    describe('Función esPar', () => {
+        
+        it('El numero 4 es par', () => {
+            assert.strictEqual(esPar(4), true);
+        });
 
-// Prueba 2: Es impar?
-if (esPar(7) !== false) {
-  console.error("Error: 7 no debería ser par");
-  process.exit(1);
-}
+        it('El numero 7 es impar', () => {
+            assert.strictEqual(esPar(7), false);
+        });
 
-// Prueba 3: ES Cero?
-if (esPar(0) !== true) {
-  console.error("Error: 0 debería ser par");
-  process.exit(1);
-}
+        it('El cero es par', () => {
+            assert.strictEqual(esPar(0), true);
+        });
 
-console.log("los tests pasaron OK");
-process.exit(0);
+    });
+});
